70268a0719d3384ce0a1f3c5e7aa215043c0dfc1
Grady Peck <gradypeck717@gmail.com>
2022-06-09 22:15:55 -0400

    added toggles for excluding absent students

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let students = [
     "Case, John",
     "Chartier, Casey",
     "Chowdhury, Wardeh",
-    // "Debas, Meron",
+    "Debas, Meron",
     "Fletcher, Billy",
     "Haggerty, Brenden",
     "Hall, Bradley",
@@ -19,12 +19,12 @@ let students = [
     "Mccool, Barbara",
     "Mcmillan, Edward",
     "Methner, Isaac",
-    // "Mitchell, Madeleine",
+    "Mitchell, Madeleine",
     "Moody, Justin",
     "Mucene, Jase",
     "Nobles, Jalean",
-    // "Pargas Paredes, David",
-    // "Roby, Travond",
+    "Pargas Paredes, David",
+    "Roby, Travond",
     "Romero, Esmeralda",
     "Schulte, Doug",
     "Strozier, Rashida",
@@ -33,6 +33,25 @@ let students = [
 ]
 
 let resultElement = document.getElementById("result");
+let studentsElement = document.getElementById("studentList");
+
+for (let i = 0; i < students.length; i++) {
+    let newEl = document.createElement("button");
+    newEl.innerText = students[i];
+    newEl.setAttribute("onclick", "toggleAbsent(this)");
+    studentsElement.appendChild(newEl);
+}
+
+function toggleAbsent(nod) {
+    if (students.includes(nod.innerText)) {
+        students.splice(students.indexOf(nod.innerText), 1);
+        nod.classList.add("absent");
+    } else {
+        students.push(nod.innerText);
+        nod.classList.remove("absent");
+    }
+    
+}
 
 function randomStudent() {
     randex = Math.floor(Math.random() * students.length);
